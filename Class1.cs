5c10f0d24288d3149013ed580aad7f618688202b
nathanf10994 <nathanf10994@gmail.com>
2024-03-06 08:36:36 -0300

    primeiro commit

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginLinkedinRPA
+{
+    public class Class1
+    {
+        public IEnumerable <string> Parametros { get; private set; }
+        public string Url { get; private set; }
+        public ChromeDriver driver { get; private set; }
+
+        public Class1(string user, string password)
+        {
+            Parametros = new List<string> { user, password };
+            Url = "https://www.linkedin.com/home";
+            driver = new ChromeDriver();
+        }
+        public void Executar()
+        {
+            driver.Navigate().GoToUrl(Url);
+            driver.FindElement(By.XPath("//*[@id=\'session_key\']")).SendKeys(Parametros.ElementAtOrDefault(0));
+            driver.FindElement(By.XPath("//*[@id=\'session_password\']")).SendKeys(Parametros.ElementAtOrDefault(1));
+            driver.FindElement(By.XPath("//*[@id=\"main-content\"]/section[1]/div/div/form/div[2]/button")).Click();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LoginLinkedinRPA;
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+        string user = " <email> "; // editar com as informações correspondentes
+        string password = " <senha> ";
+        
+            Class1 web = new Class1(user, password);
+
+            web.Executar();
+        }
+    }
+
